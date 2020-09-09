70e9db6b2330906603ade79871c1531432cab682
o-xian <onoyama1999910@gmail.com>
2020-09-09 15:29:34 +0900

    Add sidebar and links

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -3,18 +3,26 @@ module.exports = {
   themeConfig: {
     description: "ここにはディスクリプションを入れます。",
     nav: [
-      { text: "About", link: "/about/" } // ここはnav barのメニュー表示/aboutページは後ほど
-    ]
+      { text: "About", link: "/about/" },
+      {
+        text: 'Link',
+        items: [
+          { text: 'twitter', link: 'https://twitter.com/osshan9' },
+          { text: 'Github', link: 'https://github.com/o-xian999' },
+        ],
+      },
+    ],
+    sidebar: 'auto'
   },
   plugins: [
     "@vuepress/blog",
     {
       // Unique ID of current classification
-        id: 'post',
-        // Target directory
-        dirname: '_posts',
-        // Path of the `entry page` (or `list page`)
-        path: '/',
+      id: 'post',
+      // Target directory
+      dirname: 'posts',
+      // Path of the `entry page` (or `list page`)
+      path: '/',
     }
   ]
 };
